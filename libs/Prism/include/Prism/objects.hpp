d4f6eb724a4b7e0cf9e8b9de56a2ea3ec5eacb69
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2025-06-09 13:37:07 -0300

    feat(core): Implement Object abstract base class and HitRecord
    This commit introduces the `Object` abstract base class, which will serve as the common interface for all renderable objects in the scene (e.g., Sphere, Plane).
    
    It also defines the `HitRecord` struct to store detailed information about ray-object intersections, which is essential for shading and rendering.

---
+++ libs/Prism/include/Prism/objects.hpp
@@ -0,0 +1,45 @@
+#ifndef PRISM_OBJECT_HPP_
+#define PRISM_OBJECT_HPP_
+
+#include "prism_export.h"
+#include "Prism/point.hpp"
+#include "Prism/vector.hpp"
+
+namespace Prism {
+
+using ld = long double;
+
+class Ray; // Forward declaration of Ray class
+class Material; // Forward declaration of Material class
+
+struct HitRecord {
+    Point3 p;
+    Vector3 normal;
+    ld t;
+    Material* material;
+    bool front_face;
+
+    inline void set_face_normal(const Ray& ray, const Vector3& outward_normal) {
+        front_face = ray.direction().dot(outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+class PRISM_EXPORT Object {
+  public:
+    virtual ~Object() = default;
+
+    /**
+     * @brief Checks if a ray hits the object within a given distance range.
+     * @param ray The ray to test.
+     * @param t_min The minimum distance for a valid hit.
+     * @param t_max The maximum distance for a valid hit.
+     * @param rec The hit record to be filled upon a collision.
+     * @return True if a valid hit was found, false otherwise.
+     */
+    virtual bool hit(const Ray& ray, ld t_min, ld t_max, HitRecord& rec) const = 0;
+};
+
+} // namespace Prism
+
+#endif // PRISM_OBJECT_HPP_
