3e587d96d72db3724aa396a91e181bab77b6fe85
tomclima <tomecostalima1228@gmail.com>
2025-06-10 01:35:16 -0300

    feat: Grahm Schimdt Orthonormalization for 3d Given a vector v, generates a basis that contains v' = v.normalize() will be useful for defining the camera coordinate system also added testing for the function in tests/Utils.cpp

--- libs/Prism/include/Prism/utils.hpp
+++ libs/Prism/include/Prism/utils.hpp
@@ -3,10 +3,13 @@
 
 #include "prism_export.h"
 #include "Prism/point.hpp"
+#include "Prism/vector.hpp"
+#include "Prism/matrix.hpp"
 #include <initializer_list>
 
 namespace Prism {
     Point3 PRISM_EXPORT centroid(const std::initializer_list<Point3>& points);
+    Matrix<ld> PRISM_EXPORT orthonormalBasisContaining(const Vector3& vec);
 } // namespace Prism
 
 
